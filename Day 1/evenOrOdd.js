05b59d3ceaf9a24fa0a60042d2351d6a2a2684aa
Bima Mahendra <Bimahendra00@gmail.com>
2024-02-01 22:44:21 +0700

    day 1/366

---
+++ Day 1/evenOrOdd.js
@@ -0,0 +1,10 @@
+//DAY 1 Challenge
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function evenOrOdd(number) {
+  if(number %2==0){
+    return "Even"
+  }else
+  return "Odd"
+}
+console.log(evenOrOdd(3))
